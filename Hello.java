6f11091c132dbe76fcb23c9a429077cd1d4e8e2f
Sharif-Islam11 <mohammedshorif519@gmail.com>
2026-02-27 12:25:18 +0600

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,11 +1,8 @@
-import java.util.*;
 public class Hello {
 
     public static void main(String[] args) {
-        Scanner input= new Scanner(System.in);
-        System.out.print("enter n: ");
-        int a= input.nextInt();
-        System.out.println(a);
+        System.out.println("Hello world");
        
     }
+
 }
